886291e58a11efa9817eba6e8c5532bc8f032562
EddieHandford <47256888+EddieHandford@users.noreply.github.com>
2019-03-08 15:30:15 +0000

    Add files via upload

---
+++ connect 4.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar  8 14:06:52 2019
+
+@author: ED
+"""
+
+
+
+
+#Create blank array
+#7 columns 
+#6 rows
+
+board = np.zeros((6,7))
+
+
+#3 states of each cell
+#0 - blank cell
+#1 - player 1 circle
+#2 - player 2 circle
+
+#create function to output move options - eg. dont give option if colomn is full
+
+#create " move function "
+#inputs "player number , coloumn number , current array state"
+#output "new array"
+
+#create function to check for win
+#check for lines of length 4 in horizontal
+#check for lines of length 4 in vertical
+#check for diagnoal lines of lenght 4 
+#output "end game , player "most recent to move" wins
+
+
